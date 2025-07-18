2b539015b1b5bf7f30ae6eef85b67c14be33a4ab
nrslearning <nrslearning@gmail.com>
2025-07-18 15:52:26 +0530

    Update backend_main.py

--- app/backend_main.py
+++ app/backend_main.py
@@ -1,7 +1,7 @@
 
-# import sys
-# import os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi import FastAPI
 from pydantic import BaseModel
